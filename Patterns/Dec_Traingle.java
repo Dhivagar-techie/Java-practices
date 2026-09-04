aa54ef973a4cf9b70150d236c67e90fc21cf4ddb
thanosda <154046417+Thanosda@users.noreply.github.com>
2026-09-05 02:56:10 +0530

    code added

---
+++ Patterns/Dec_Traingle.java
@@ -0,0 +1,12 @@
+public class Dec_Traingle {
+    public static void main(String[] args) {
+         for (int i=0;i<=5;i++){
+            for (int j=i;j<=5;j++){
+                System.out.print("*  ");
+
+            }
+         System.out.println();
+        }
+    }
+    
+}
---
+++ Patterns/square.java
@@ -0,0 +1,16 @@
+public class square {
+    public static void main(String[] args) {
+        
+    
+    
+
+for (int i = 1; i <= 4; i++) {       // rows
+
+    for (int j = 1; j <= 4; j++) {   // columns
+        System.out.print("* ");
+    }
+
+    System.out.println();            // next row
+}
+}
+}
