e662f10a223b8a84890e6279e40e725f5f44ac24
thanosda <154046417+Thanosda@users.noreply.github.com>
2026-09-23 14:50:51 +0530

    added

--- Patterns/Inc_Trainagle.java
+++ Patterns/Inc_Trainagle.java
@@ -1,13 +1,33 @@
+
+import java.util.Scanner;
+
 public class Inc_Trainagle {
     public static void main(String[] args) {
-        
-        for (int i=0;i<=4;i++){
+    System.out.println("this is the triangle of normal");   
+    traingle();
+    System.out.println("---------");
+    System.out.println("traingle with numbers");
+    tri_n(6);
+
+
+
+    }  
+    public static void traingle() {
+          for (int i=0;i<=4;i++){
             for (int j=0;j<=i;j++){
                 System.out.print("* ");
 
             }
          System.out.println();
         }
+        
     }
+
+    public static void tri_n(int n) {
+
+
     
+        
+    }
+          
 }
