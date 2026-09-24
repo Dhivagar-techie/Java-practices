4ba4e76a056812bd49212ddfef82be2a6e5d2f38
thanosda <154046417+Thanosda@users.noreply.github.com>
2026-09-25 00:35:07 +0530

    added

--- Basics/LeapYear.java
+++ Basics/LeapYear.java
@@ -1,6 +1,36 @@
 import java.util.*;
+
 public class LeapYear {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        System.out.println("enter the year");
+
+        sample(year);
+        sample1(year);
+    }
+
+    public static void sample(int year) {
+        if (year % 400 == 0) {
+            System.out.println("Leap Year");
+        } else if (year % 100 == 0) {
+            System.out.println("Not a Leap Year");
+        } else if (year % 4 == 0) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Not a Leap Year");
+        }
+    }
+
+    public static void sample1(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Not a Leap Year");
+        }
+
     }
 }
